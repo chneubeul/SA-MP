745853ad2f11738f5c2f1687c0d6f7d5ba71150d
AbyssMorgan <wojownikx9x@gmail.com>
2016-06-13 22:22:33 +0200

    Fix Object Convert Script

--- include/SAM/IMG Generator/object_convert.cmd
+++ include/SAM/IMG Generator/object_convert.cmd
@@ -6,19 +6,24 @@ COLOR 9F
 ECHO.
 ECHO Binary Object File Converter v1.0 by Abyss Morgan
 ECHO Drop input file, and click enter.
+:RESTART
 ECHO.
+SET FILE=
 SET /P FILE=Input file: 
 ECHO.
-IF EXIST "%FILE%" (
-	CALL :CONVERT "%FILE%"
-) ELSE (
+
+IF EXIST %FILE% (
 	CALL :CONVERT %FILE%
+) ELSE (
+	CALL :CONVERT "%FILE%"
 )
 ECHO.
-PAUSE
-GOTO :eof
+
+
+GOTO :RESTART
 
 :CONVERT
+IF /I "%~1" == "exit" EXIT
 IMG_Gen	"%~1" "%CD%\%~n1.bin"
 ECHO.
 ECHO Output file: %CD%\%~n1.bin
